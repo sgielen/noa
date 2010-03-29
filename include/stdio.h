51817bb89ec58c1b9570ebd848a6aba7ccb8e1fc
Ed Schouten <ed@80386.nl>
2010-03-29 19:47:23 +0200

    Also add an URL.

--- include/stdio.h
+++ include/stdio.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2010 The Noa Project
+ * Copyright (c) 2010 The Noa Project, http://noaproject.org/
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
