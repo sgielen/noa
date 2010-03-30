4b017227e4af67e60a9920e1dd502c64e4ca67cf
Ed Schouten <ed@80386.nl>
2010-03-30 14:50:34 +0200

    Add __S_IF*-flags.

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -56,29 +56,38 @@ struct stat {
 #define	st_mtime	st_mtim.tv_sec
 #define	st_ctime	st_ctim.tv_sec
 
-#define	S_IXOTH		00001
-#define	S_IWOTH		00002
-#define	S_IROTH		00004
-#define	S_IRWXO		00007
-#define	S_IXGRP		00010
-#define	S_IWGRP		00020
-#define	S_IRGRP		00040
-#define	S_IRWXG		00070
-#define	S_IXUSR		00100
-#define	S_IWUSR		00200
-#define	S_IRUSR		00400
-#define	S_IRWXU		00700
-#define	S_ISVTX		01000
-#define	S_ISGID		02000
-#define	S_ISUID		04000
+#define	S_IXOTH		0x00001
+#define	S_IWOTH		0x00002
+#define	S_IROTH		0x00004
+#define	S_IRWXO		(S_IXOTH|S_IWOTH|S_IROTH)
+#define	S_IXGRP		0x00008
+#define	S_IWGRP		0x00010
+#define	S_IRGRP		0x00020
+#define	S_IRWXG		(S_IXGRP|S_IWGRP|S_IRGRP)
+#define	S_IXUSR		0x00040
+#define	S_IWUSR		0x00080
+#define	S_IRUSR		0x00100
+#define	S_IRWXU		(S_IXUSR|S_IWUSR|S_IRUSR)
+#define	S_ISVTX		0x00200
+#define	S_ISGID		0x00400
+#define	S_ISUID		0x00800
 
-#define	S_ISBLK(m)	0
-#define	S_ISCHR(m)	TODO
-#define	S_ISDIR(m)	TODO
-#define	S_ISFIFO(m)	TODO
-#define	S_ISREG(m)	TODO
-#define	S_ISLNK(m)	TODO
-#define	S_ISSOCK(m)	TODO
+#define	S_IFMT		0xf0000
+#define	__S_IFBLK	0x10000
+#define	__S_IFCHR	0x20000
+#define	__S_IFIFO	0x30000
+#define	__S_IFREG	0x40000
+#define	__S_IFDIR	0x50000
+#define	__S_IFLNK	0x60000
+#define	__S_IFSOCK	0x70000
+
+#define	S_ISBLK(m)	(((m) & S_IFMT) == __S_IFBLK)
+#define	S_ISCHR(m)	(((m) & S_IFMT) == __S_IFCHR)
+#define	S_ISDIR(m)	(((m) & S_IFMT) == __S_IFIFO)
+#define	S_ISFIFO(m)	(((m) & S_IFMT) == __S_IFDIR)
+#define	S_ISREG(m)	(((m) & S_IFMT) == __S_IFREG)
+#define	S_ISLNK(m)	(((m) & S_IFMT) == __S_IFLNK)
+#define	S_ISSOCK(m)	(((m) & S_IFMT) == __S_IFSOCK)
 #define	S_TYPEISMQ(m)	TODO
 #define	S_TYPEISSEM(m)	TODO
 #define	S_TYPEISSHM(m)	TODO
