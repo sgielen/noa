8c4eccaa2ae76913b1ed4ef6ac68eb089952f9b5
Ed Schouten <ed@80386.nl>
2010-04-02 23:56:09 +0200

    Make library build work again.

--- share/mk/noa.lib.mk
+++ share/mk/noa.lib.mk
@@ -1,10 +1,19 @@
 .include <noa.sys.mk>
 
+CFLAGS+=-fpic
+
+LDFLAGS+=-Wl,--version-script=Version.map -nostdlib -shared -Wl,-x
+
 OBJS+=	${SRCS:N*.h:R:S/$/.o/}
 
 all: lib${LIB}.so
 
-lib${LIB}.so: ${OBJS}
+Version.map: ${VERSION_DEF} ${SYMBOL_MAPS}
+	cat ${SYMBOL_MAPS} | awk -v vfile=${VERSION_DEF} \
+		-f /usr/share/mk/version_gen.awk > ${.TARGET}
+
+lib${LIB}.so: ${OBJS} Version.map
+	${CC} ${LDFLAGS} -o ${.TARGET} ${OBJS}
 
 ${OBJS}: ${SRCS:M*.h}
 
