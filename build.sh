4c5116c7af672651029c259f78bd79becf092229
Ed Schouten <ed@80386.nl>
2010-04-02 23:57:43 +0200

    Put -nostdinc in build.sh, where it belongs.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-export CFLAGS='-O3 -I../../include -I../../include/machdep/${MACHINE}'
+export CFLAGS='-O3 -nostdinc -I../../include -I../../include/machdep/${MACHINE}'
 make -C lib/libc -m ../../share/mk $1
--- share/mk/noa.sys.mk
+++ share/mk/noa.sys.mk
@@ -6,9 +6,9 @@ MACHINE=x86_64
 MACHINE=x86_32
 .endif
 
-CFLAGS+=-nostdinc -std=gnu99 -Wsystem-headers -Werror -Wall \
-	-Wno-format-y2k -W -Wno-unused-parameter -Wstrict-prototypes \
-	-Wmissing-prototypes -Wpointer-arith -Wreturn-type -Wcast-qual \
-	-Wwrite-strings -Wswitch -Wshadow -Wunused-parameter \
-	-Wcast-align -Wchar-subscripts -Winline -Wnested-externs \
-	-Wredundant-decls -Wold-style-definition -Wno-pointer-sign
+CFLAGS+=-std=gnu99 -Wsystem-headers -Werror -Wall -Wno-format-y2k -W \
+	-Wno-unused-parameter -Wstrict-prototypes -Wmissing-prototypes \
+	-Wpointer-arith -Wreturn-type -Wcast-qual -Wwrite-strings -Wswitch \
+	-Wshadow -Wunused-parameter -Wcast-align -Wchar-subscripts -Winline \
+	-Wnested-externs -Wredundant-decls -Wold-style-definition \
+	-Wno-pointer-sign \
