0a8f35e814636b7f0ca24bdd8de63e54a1c51b7e
Ed Schouten <ed@80386.nl>
2010-04-02 23:59:07 +0200

    Use absolute paths for the inclusions.

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-export CFLAGS='-O3 -nostdinc -I../../include -I../../include/machdep/${MACHINE}'
+HERE="`realpath .`"
+export CFLAGS="-O3 -nostdinc -I$HERE/include -I$HERE/include/machdep/\${MACHINE}"
 make -C lib/libc -m ../../share/mk $1
