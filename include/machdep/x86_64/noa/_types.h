f3560932b9e17e876e94a81fd52fad14950c61ac
Ed Schouten <ed@80386.nl>
2010-04-02 23:06:46 +0200

    Fix typos.

--- include/machdep/x86_64/noa/_types.h
+++ include/machdep/x86_64/noa/_types.h
@@ -28,9 +28,9 @@
 #define	_MACHDEP_NOA_TYPES_H_
 
 typedef signed char __int8_t;
-typedef unsigned char __int8_t;
+typedef unsigned char __uint8_t;
 typedef signed short __int16_t;
-typedef unsigned short __int16_t;
+typedef unsigned short __uint16_t;
 typedef signed int __int32_t;
 typedef unsigned int __uint32_t;
 typedef signed long __int64_t;
