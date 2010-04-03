949bf0e937ebc08b3ce83e0ea28e93e210ada29f
Ed Schouten <ed@80386.nl>
2010-04-03 21:22:46 +0200

    Properly use $@.

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 HERE="`realpath .`"
 export CFLAGS="-O3 -nostdinc -I$HERE/include -I$HERE/include/machdep/\${MACHINE}"
 SHAREMK="`realpath share/mk`"
-make -C lib/libc -m $SHAREMK $1
-make -C lib/libteken -m $SHAREMK $1
-#make -C kernel -m $SHAREMK $1
+make -C lib/libc -m $SHAREMK "$@"
+make -C lib/libteken -m $SHAREMK "$@"
+#make -C kernel -m $SHAREMK "$@"
