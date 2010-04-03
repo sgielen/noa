d05408185873833bcebea00bca67826e675e4c8b
Ed Schouten <ed@80386.nl>
2010-04-03 23:05:28 +0200

    Add pretty printing.

--- share/mk/noa.subdir.mk
+++ share/mk/noa.subdir.mk
@@ -1,6 +1,8 @@
 _SUBDIR: .USE
-	for entry in ${SUBDIR}; do \
-		${MAKE} -C ${.CURDIR}/$${entry} ${.TARGET}; \
+	@for entry in ${SUBDIR}; do \
+		echo "===> ${DIRPRFX}$$entry (${.TARGET})"; \
+		${MAKE} -C ${.CURDIR}/$${entry} ${.TARGET} \
+		    DIRPRFX=${DIRPRFX}$${entry}/; \
 	done
 
 .for __target in all clean
