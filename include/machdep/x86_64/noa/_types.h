977af85293a3101a0cba51eb552b64690a592560
Ed Schouten <ed@80386.nl>
2010-04-03 20:06:19 +0200

    Just let each architecture have its own <stdint.h>. Lot simpler.

--- include/machdep/x86_64/noa/_types.h
+++ include/machdep/x86_64/noa/_types.h
@@ -27,32 +27,9 @@
 #ifndef _MACHDEP_NOA_TYPES_H_
 #define	_MACHDEP_NOA_TYPES_H_
 
-typedef signed char __int8_t;
-typedef unsigned char __uint8_t;
-typedef signed short __int16_t;
-typedef unsigned short __uint16_t;
 typedef signed int __int32_t;
 typedef unsigned int __uint32_t;
 typedef signed long __int64_t;
-typedef unsigned long __uint64_t;
-
-typedef __int8_t __int_least8_t;
-typedef __uint8_t __uint_least8_t;
-typedef __int16_t __int_least16_t;
-typedef __uint16_t __uint_least16_t;
-typedef __int32_t __int_least32_t;
-typedef __uint32_t __uint_least32_t;
-typedef __int64_t __int_least64_t;
-typedef __uint64_t __uint_least64_t;
-
-typedef __int32_t __int_fast8_t;
-typedef __uint32_t __uint_fast8_t;
-typedef __int32_t __int_fast16_t;
-typedef __uint32_t __uint_fast16_t;
-typedef __int32_t __int_fast32_t;
-typedef __uint32_t __uint_fast32_t;
-typedef __int64_t __int_fast64_t;
-typedef __uint64_t __uint_fast64_t;
 
 typedef unsigned long __size_t;
 typedef signed long __ssize_t;
