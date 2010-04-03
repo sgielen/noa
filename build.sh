6c2fe40e4d821906fdea1c3884a871cfb1a22df0
Ed Schouten <ed@80386.nl>
2010-04-03 08:33:18 +0200

    Use an absolute path for share/mk.

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -e
 
 HERE="`realpath .`"
 export CFLAGS="-O3 -nostdinc -I$HERE/include -I$HERE/include/machdep/\${MACHINE}"
-make -C lib/libc -m ../../share/mk $1
+SHAREMK="`realpath share/mk`"
+make -C lib/libc -m $SHAREMK $1
