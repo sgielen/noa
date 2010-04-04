1fc6c365a82b7ce130247c3ab4418e38deaeca54
Ed Schouten <ed@80386.nl>
2010-04-04 18:18:41 +0200

    Make whitespace more consistent with select().

--- lib/libc/time/utimes.c
+++ lib/libc/time/utimes.c
@@ -37,6 +37,5 @@ utimes(const char *path, const struct timeval _times[2])
 	times[0].tv_nsec = _times[0].tv_usec * 1000;
 	times[1].tv_sec = _times[1].tv_sec;
 	times[1].tv_nsec = _times[1].tv_usec * 1000;
-
 	return (utimensat(AT_FDCWD, path, times, 0));
 }
