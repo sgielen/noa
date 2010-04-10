29fc6e63975d65b0f1826a413cc0fff237fe8781
Ed Schouten <ed@80386.nl>
2010-04-11 00:16:36 +0200

    Whitespace.

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -39,16 +39,16 @@
 #include <noa/types.h>
 
 __ABI_STRUCT(stat, 128, {
-	dev_t	st_dev;
-	ino_t	st_ino;
-	mode_t	st_mode;
-	nlink_t	st_nlink;
-	uid_t	st_uid;
-	gid_t	st_gid;
-	off_t	st_size;
-	struct timespec st_atim;
-	struct timespec st_mtim;
-	struct timespec st_ctim;
+	dev_t		st_dev;
+	ino_t		st_ino;
+	mode_t		st_mode;
+	nlink_t		st_nlink;
+	uid_t		st_uid;
+	gid_t		st_gid;
+	off_t		st_size;
+	struct timespec	st_atim;
+	struct timespec	st_mtim;
+	struct timespec	st_ctim;
 })
 
 #define	st_atime	st_atim.tv_sec
