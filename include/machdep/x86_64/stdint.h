92ccfa208137d478f27dc12e730155dd79d0667a
Ed Schouten <ed@80386.nl>
2010-04-10 12:36:57 +0200

    Undo the previous commit.
    We shouldn't add these, since it implies we need to kick intmax_t.

--- include/machdep/x86_64/stdint.h
+++ include/machdep/x86_64/stdint.h
@@ -37,8 +37,6 @@ typedef signed int int32_t;
 typedef unsigned int uint32_t;
 typedef signed long int64_t;
 typedef unsigned long uint64_t;
-typedef signed int int128_t __attribute__((mode(TI)));
-typedef unsigned int uint128_t __attribute__((mode(TI)));
 
 typedef int8_t int_least8_t;
 typedef uint8_t uint_least8_t;
@@ -48,8 +46,6 @@ typedef int32_t int_least32_t;
 typedef uint32_t uint_least32_t;
 typedef int64_t int_least64_t;
 typedef uint64_t uint_least64_t;
-typedef int128_t int_least128_t;
-typedef uint128_t uint_least128_t;
 
 typedef int32_t int_fast8_t;
 typedef uint32_t uint_fast8_t;
@@ -59,8 +55,6 @@ typedef int32_t int_fast32_t;
 typedef uint32_t uint_fast32_t;
 typedef int64_t int_fast64_t;
 typedef uint64_t uint_fast64_t;
-typedef int128_t int_fast128_t;
-typedef uint128_t uint_fast128_t;
 
 typedef int64_t intptr_t;
 typedef uint64_t uintptr_t;
