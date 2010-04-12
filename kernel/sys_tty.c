0653eb67d0df534e574496cab50885d1d9f3b1ab
Ed Schouten <ed@80386.nl>
2010-04-12 17:06:24 +0200

    Fix stupid regex fault in copyright.

--- kernel/sys_tty.c
+++ kernel/sys_tty.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2010 The Noa Project, http://noap __unusedroject.org/
+ * Copyright (c) 2010 The Noa Project, http://noaproject.org/
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
